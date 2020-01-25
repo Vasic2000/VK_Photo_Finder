0308e9716060750babac908f7e68e98d178f7a8f
Vasic2000 <vasic2000@yandex.ru>
2020-01-25 14:52:17 +0300

    Init

---
+++ app/src/main/java/cz/vasic2000/vk_photo_finder/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package cz.vasic2000.vk_photo_finder.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/cz/vasic2000/vk_photo_finder/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package cz.vasic2000.vk_photo_finder.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
